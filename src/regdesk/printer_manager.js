6940308fa6f2fb4a9307a9a5fdcbaec217eb525e
Cellivar <cellivar.kangaroorat@gmail.com>
2021-12-16 02:37:14 -0800

    Add printer manager and harness code for testing

--- src/regdesk/printer_manager.js
+++ src/regdesk/printer_manager.js
@@ -1,32 +1,66 @@
-import { LabelEpl, LP2844 } from 'C:/Git/WebZLP/src/index.js'
+import { LabelEpl, LP2844 } from 'WebZLP/src/LP2844'
 
 export class PrinterManager {
     #devices = [];
     #printers = [];
 
-    primaryPrinter;
-    secondaryPrinter;
+    primaryPrinter = 0;
+    secondaryPrinter = 1;
 
     #printerType = LP2844;
 
     constructor() {
     }
 
-    getLabel() { return new LabelEpl(0, 0, 0); }
+    get printers() { return this.#printers; }
 
     async printerConnect(e) {
-
+        if (!this.#devices.includes(e.device)) {
+            this.#devices.push(e.device);
+            let printer = new this.#printerType(e.device);
+            await printer.connect();
+            this.#printers.push(printer);
+            this.primaryPrinter = 0;
+        }
     }
 
     async printerDisconnect(e) {
-
+        const idx = this.#devices.findIndex(i => e.device == i);
+        if (idx > -1) {
+            const p = this.#printers[idx];
+            this.#devices.splice(idx, 1);
+            this.#printers.splice(idx, 1);
+            this.resetPrinterButtons();
+            await p.dispose();
+        }
     }
 
-    async printerPair(e) {
+    async printerPair() {
+        try {
+            let device = await navigator.usb.requestDevice({
+                filters: [
+                    { vendorId: this.#printerType.usbVendorId }
+                ]
+            });
 
+            await this.printerConnect( { device : device} )
+        } catch (e) {
+            console.log("Failed to connect to printer!");
+            console.log(e);
+            return;
+        }
     }
 
     async printerReload(e) {
+        this.#devices = [];
+        this.#printers.forEach(p => p.dispose());
+        this.#printers = [];
 
+        navigator.usb.getDevices().then((printers) => {
+            printers.forEach(async d => {
+                await this.printerConnect( { device : d });
+            });
+        });
     }
+
 }
