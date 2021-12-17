fce29c7ddaabe7f0dc963178701e7c19e7e418de
kobaj <kobaj.g@gmail.com>
2021-12-17 12:50:09 -0800

    Feature/kobaj unit test all the things (#7)
    * Put all the message methods into a single place.
    
    * Add a small library to know when running in watch vs build.
    
    * Add sinon-chrome and other dependencies for future testing.
    
    * Unit test and update disable_auto_logout.
    
    * Unit test and update event_page.
    
    * Unit test and update popup.
    
    * Update comment on disable_auto_logout implementation.

--- src/event_page.js
+++ src/event_page.js
@@ -1,39 +1,45 @@
 import { get } from 'lodash-es';
 
+import { isScript } from './startup/startup.js';
+import { sendPopupAMessage, sendTabAMessage, MESSAGE_TYPE } from './coms/communication.js';
+
+const STORAGE_KEY_DISABLE_AUTO_LOGOUT = 'disable-auto-logout-storage';
+
 const getCurrentTab = async () => {
-  let queryOptions = { active: true, currentWindow: true };
-  let [tab] = await chrome.tabs.query(queryOptions);
+  const queryOptions = { active: true, currentWindow: true };
+  const [tab] = await chrome.tabs.query(queryOptions);
   return tab;
 };
 
-const sendTabAMessage = (tab, type, payload) => {
-  chrome.tabs.sendMessage(tab.id, { type, payload });
+const init = () => {
+  chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
+    const tab = await getCurrentTab();
+
+    if (request.type === MESSAGE_TYPE.printLegacy) {
+      // TODO, should probably just use content scripts?
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: ['content_script.js'],
+      });
+    }
+
+    if (request.type === MESSAGE_TYPE.finishLoad
+      && (request.payload === 'disable-auto-logout' || request.payload === 'popup')) {
+      const isEnabled = get(await chrome.storage.local.get([STORAGE_KEY_DISABLE_AUTO_LOGOUT]), [STORAGE_KEY_DISABLE_AUTO_LOGOUT], false);
+      sendTabAMessage(tab, MESSAGE_TYPE.changeLogout, isEnabled);
+      sendPopupAMessage(MESSAGE_TYPE.changeLogout, isEnabled);
+    }
+
+    if (request.type === MESSAGE_TYPE.changeLogout) {
+      await chrome.storage.local.set({ [STORAGE_KEY_DISABLE_AUTO_LOGOUT]: request.payload });
+      sendTabAMessage(tab, request.type, request.payload);
+    }
+  });
 }
 
-const sendPopupAMessage = (type, payload) => {
-  chrome.runtime.sendMessage({ type, payload });
+if (isScript()) {
+  // When running as a script (on a webpage) auto execute.
+  init();
 }
 
-chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
-  const tab = await getCurrentTab();
-
-  if (request.type === 'print-legacy-plz') {
-    // TODO, should probably just use content scripts?
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      files: ["content_script.js"],
-    });
-  }
-
-  if (request.type === 'loaded-plz'
-    && (request.payload === 'disable-auto-logout' || request.payload === 'popup')) {
-    const isEnabled = get(await chrome.storage.local.get(['disable-auto-logout-storage']), 'disable-auto-logout-storage', false);
-    sendTabAMessage(tab, 'change-logout-plz', isEnabled);
-    sendPopupAMessage('change-logout-plz', isEnabled);
-  }
-
-  if (request.type === 'change-logout-plz') {
-    await chrome.storage.local.set({ 'disable-auto-logout-storage': request.payload });
-    sendTabAMessage(tab, request.type, request.payload);
-  }
-});
+export { init };
