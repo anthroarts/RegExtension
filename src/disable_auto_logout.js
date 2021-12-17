3aad6c778ce33d84276feae5ace87be9a0880274
Kobaj <kobaj.g@gmail.com>
2021-12-16 16:44:19 -0800

    Unit test and update disable_auto_logout.

--- src/disable_auto_logout.js
+++ src/disable_auto_logout.js
@@ -1,23 +1,32 @@
-(function () {
+import { isScript } from './startup/startup.js'
+import { sendBackgroundScriptAMessage, MESSAGE_TYPE } from './coms/communication.js';
+
+const init = () => {
   var isEnabled = false;
 
-  const intervalID = setInterval(() => {
+  setInterval(() => {
     if (!isEnabled) {
       return;
     }
 
+    // There is probably a better way to do this, utilizing something like
+    // angular.element($('[ng-controller]')).scope().<something>.clearIdle()
+    // But I couldn't figure it out. And hey, this works!
     window.dispatchEvent(new KeyboardEvent('keyup', null));
   }, 1000);
 
   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.type === 'change-logout-plz') {
+    if (request.type === MESSAGE_TYPE.changeLogout) {
       isEnabled = request.payload;
     }
   });
 
-  const sendBackgroundScriptAMessage = (type, payload) => {
-    chrome.runtime.sendMessage({ type, payload });
-  }
+  sendBackgroundScriptAMessage(MESSAGE_TYPE.finishLoad, 'disable-auto-logout');
+};
 
-  sendBackgroundScriptAMessage('loaded-plz', 'disable-auto-logout');
-})();
+if (isScript()) {
+  // When running as a script (on a webpage) auto execute.
+  init();
+}
+
+export { init };
