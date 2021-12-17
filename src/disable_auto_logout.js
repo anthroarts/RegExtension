426dca4dba89c91942bf189f4c55271800d1437d
Kobaj <kobaj.g@gmail.com>
2021-12-17 11:03:50 -0800

    Update comment on disable_auto_logout implementation.

--- src/disable_auto_logout.js
+++ src/disable_auto_logout.js
@@ -9,9 +9,17 @@ const init = () => {
       return;
     }
 
-    // There is probably a better way to do this, utilizing something like
-    // angular.element($('[ng-controller]')).scope().<something>.clearIdle()
-    // But I couldn't figure it out. And hey, this works!
+    // Inside regfox is a method you can grep called `clearIdle`. It resets 
+    // the idle timers and if not called, will lead to the auto-logout popup. 
+    // 
+    // Ideally we would call this `clearIdle` directly, but its obfuscated 
+    // behind Angular. Theoretically we could grab a reference via something like
+    // `angular.element($('[ng-controller]')).scope().<something>.clearIdle()`
+    // But I couldn't figure it out.
+    //
+    // However, inside regfox is a window.addEventListener that triggers on 
+    // keyup, mousemove, and a few other window.Events and calls `clearIdle` 
+    // for us! So while a bit roundabout, this does reset idle timers too.
     window.dispatchEvent(new KeyboardEvent('keyup', null));
   }, 1000);
 
