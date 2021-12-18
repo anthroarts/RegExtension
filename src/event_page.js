2edbbee29756527280c122e21007ba5a220ce20c
kobaj <kobaj.g@gmail.com>
2021-12-18 15:13:03 -0800

    Fix bug where some tabs wouldn't get messages. (#9)

--- src/event_page.js
+++ src/event_page.js
@@ -11,12 +11,16 @@ const getCurrentTab = async () => {
   return tab;
 };
 
+const getAllRegfoxTabs = async () => {
+  const queryOptions = { url: "https://manage.webconnex.com/*" };
+  return chrome.tabs.query(queryOptions);
+}
+
 const init = () => {
   chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
-    const tab = await getCurrentTab();
-
     if (request.type === MESSAGE_TYPE.printLegacy) {
       // TODO, should probably just use content scripts?
+      const tab = await getCurrentTab();
       chrome.scripting.executeScript({
         target: { tabId: tab.id },
         files: ['content_script.js'],
@@ -25,14 +29,16 @@ const init = () => {
 
     if (request.type === MESSAGE_TYPE.finishLoad
       && (request.payload === 'disable-auto-logout' || request.payload === 'popup')) {
+      const tabs = await getAllRegfoxTabs();
       const isEnabled = get(await chrome.storage.local.get([STORAGE_KEY_DISABLE_AUTO_LOGOUT]), [STORAGE_KEY_DISABLE_AUTO_LOGOUT], false);
-      sendTabAMessage(tab, MESSAGE_TYPE.changeLogout, isEnabled);
+      tabs.forEach(tab => sendTabAMessage(tab, MESSAGE_TYPE.changeLogout, isEnabled));
       sendPopupAMessage(MESSAGE_TYPE.changeLogout, isEnabled);
     }
 
     if (request.type === MESSAGE_TYPE.changeLogout) {
+      const tabs = await getAllRegfoxTabs();
       await chrome.storage.local.set({ [STORAGE_KEY_DISABLE_AUTO_LOGOUT]: request.payload });
-      sendTabAMessage(tab, request.type, request.payload);
+      tabs.forEach(tab => sendTabAMessage(tab, MESSAGE_TYPE.changeLogout, request.payload));
     }
   });
 }
